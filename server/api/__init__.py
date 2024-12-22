0917d133c2263ba89cf98b9ef46a49e819bc1448
Akshat Mishra <136343806+Adm-2005@users.noreply.github.com>
2024-12-23 00:06:13 +0530

    replaced deprecated method for initializing mobilenet weights with new method

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 CORS(app)
 
-mobilenet_v3_model = mobilenet_v3_small(pretrained=True)
+mobilenet_v3_model = mobilenet_v3_small(weights=MobileNet_V3_Small_Weights.IMAGENET1K_V1)
 tokenizer = T5Tokenizer.from_pretrained("google-t5/t5-small")
 decoder = T5ForConditionalGeneration.from_pretrained("google-t5/t5-small")
 
